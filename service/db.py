674348b5275956f0c6b2bd069c2c0ccc707f2f3a
avni0405 <avinavekariya6@gmail.com>
2021-01-09 22:07:46 +0530

    database connection done

--- service/db.py
+++ service/db.py
@@ -2,9 +2,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-def firebase_database():
+def db_connection():
     """Return the firebase object."""
-    cred = credentials.Certificate('credential.json')
+    cred = credentials.Certificate('serviceAccount.json')
     firebase_admin.initialize_app(cred)
     db = firestore.client()
     return db
