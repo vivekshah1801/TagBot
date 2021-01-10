6343981ed439d781fdc26e9422cff51b6c4ac468
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2021-01-10 15:01:38 +0530

    Add routes to detection-service

--- bot/bot.js
+++ bot/bot.js
@@ -3,13 +3,32 @@ if (process.env.NODE_ENV !== "production") {
   // - Checks if environment is profuction or development.
   require("dotenv").config();
 }
+const axios = require("axios").default;
 
 // new Client instance
-const { Client } = require("discord.js");
-const client = new Client();
+const { Client, Intents } = require("discord.js");
 
+//CONFIG
+const intents = new Intents([
+  Intents.NON_PRIVILEGED, // include all non-privileged intents, would be better to specify which ones you actually need
+  "GUILD_MEMBERS", // lets you request guild members
+]);
+const client = new Client({ ws: { intents } });
+
+// ENV VARIABLES ----------------------
 // bot token from env variable
 const BOT_TOKEN = process.env.BOT_TOKEN;
+const SERVICE_URL = process.env.SERVICE_URL;
+
+const WELCOME_MESSAGE = `---------- Hello World, I am TagBot ---------
+I use Machine Learning to automatically tag people in their images.
+
+To get Started.
+ - Send an image with only your face and tag me (@TagBot) in message-body, so that I will remember you.
+ - After that, you will be tagged in all images sent with your face in it. (! 100% guaranteed)
+
+If you want to remove your data.
+ - simply say "@TagBot !removeMyData"`;
 
 // login
 client.login(BOT_TOKEN);
@@ -24,8 +43,13 @@ client.on("ready", () => {
 //BOT joined a server --event
 client.on("guildCreate", (guild) => {
   console.log("Joined --- : " + guild.name + " ----");
-  console.log();
-  //Your other stuff like adding to guildArray
+  const channel = guild.channels.cache.find(
+    (channel) =>
+      (channel.name === "general" || channel.name === "welcome") &&
+      channel.type === "text" &&
+      channel.permissionsFor(guild.me).has("SEND_MESSAGES")
+  );
+  channel.send(WELCOME_MESSAGE);
 });
 
 //BOT removed from a server --event
@@ -38,9 +62,24 @@ client.on("guildDelete", (guild) => {
 client.on("guildMemberAdd", (member) => {
   console.log(member.id + " Joined");
 });
+
+//Delete -----------------------------------------------------
 //USER left --event
 client.on("guildMemberRemove", (member) => {
   console.log(member.id + " Left");
+  // POST-REQUEST /delete
+  axios
+    .delete(SERVICE_URL + "/delete", {
+      userId: member.id,
+      serverId: member.guild.id,
+    })
+    .then(function (response) {
+      messageRef.reply(response.message);
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 // message event
@@ -48,18 +87,82 @@ client.on("message", async (messageRef) => {
   // ignore messages sent by a bot.
   if (messageRef.author.bot) return;
 
-  // COMMANDS ---
-  // Health Check Test
+  //Status -----------------------------------------------------
   if (messageRef.content === "!status") {
-    messageRef.channel.send("Bot Status: HEALTHY");
+    axios
+      .get(SERVICE_URL + "/status")
+      .then(function (response) {
+        // handle success
+        messageRef.channel.send("Bot Status: " + response.status);
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
   }
+  const tagged = messageRef.mentions.users.has(client.user.id);
 
-  // Attachment
+  //ADD -----------------------------------------------------
+  if (tagged && messageRef.content.split(" ").indexOf("!train") !== -1) {
+    messageRef.attachments.forEach(async (attachment) => {
+      // POST-REQUEST /add
+      axios
+        .post(SERVICE_URL + "/add", {
+          userTag: messageRef.author.tag,
+          userId: messageRef.author.id,
+          serverId: messageRef.guild.id,
+          image: attachment.url,
+        })
+        .then(function (response) {
+          messageRef.reply(response.message);
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+  }
+  //removeMyData -----------------------------------------------------
+  if (tagged && messageRef.content.split(" ").indexOf("!removeMyData") !== -1) {
+    // POST-REQUEST /delete
+    axios
+      .delete(SERVICE_URL + "/delete", {
+        userId: messageRef.author.id,
+        serverId: messageRef.guild.id,
+      })
+      .then(function (response) {
+        messageRef.reply(response.message);
+        console.log(response.message);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  //DETECT -----------------------------------------------------
   messageRef.attachments.forEach(async (attachment) => {
-    // DEBUG log
-    if (process.env.NODE_ENV !== "production") {
-      console.log("[ATTACHMENT] ", attachment);
-      //- send that image to SERVICE to get discord ids to tag.
-    }
+    // POST-REQUEST /detect
+    //- send that image to SERVICE to get discord ids to tag.
+    axios
+      .post(SERVICE_URL + "/detect", {
+        userTag: messageRef.author.tag,
+        userId: messageRef.author.id,
+        serverId: messageRef.guild.id,
+        image: attachment.url,
+      })
+      .then(function (response) {
+        const tagMessageBody = "";
+
+        if (response.detected_id.length !== 0) {
+          tagMessageBody = response.detected_id.join(" @");
+        }
+        tagMessageBody = "@" + tagMessageBody;
+
+        messageRef.reply(tagMessageBody);
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   });
 });
